5cff4bcf07d4193d8578a13abdf1a681dd577abc
kemerovk <68332397+kemerovk@users.noreply.github.com>
2022-09-19 21:30:39 +0300

    Add files via upload

---
+++ Sfdgource.cpp
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <math.h>
+#include <iostream>
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+double func_1(double a, double b)
+{
+	if (tan(7 * a + sin(b)) != 0)
+	{	
+		if (1 / tan(7 * a + sin(b)) >= 0)
+		{
+			return sqrt(1 / tan(7 * a + sin(b)));
+		}
+		else
+		{
+			return 0;
+		}
+	}	
+	else
+	{
+		return 0;
+	}
+}
+
+int main() {
+	double x, y, a;
+	setlocale(LC_CTYPE, "Rus");
+
+	cout << "Введите значение а: ";
+	cin >> a;
+
+	while (a > 999999.999999 || a < -999999.999999)
+	{
+		cout << "Введите корректное число a: ";
+		cin >> a;
+	}
+	printf("---------------------------------------------------------------------------\n|        x        |           a           |                 y             |\n---------------------------------------------------------------------------\n");
+	
+	for (x = -4.2; x <= 6; x += 1.2) {
+		y = func_1(a, x);
+
+		if (y != 0)
+		{
+			printf("|%10f       |%17f      |%21.6f          |\n---------------------------------------------------------------------------\n", x, a, y);
+		}
+		else 
+		{
+			printf("|%10f       |%17f      | %21s         |\n---------------------------------------------------------------------------\n", x, a, "Невозможно");
+		}
+	}
+	return 0;
+}
